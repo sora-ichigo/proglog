cc7df088ad4524c3f4c8293e80522eec70073523
igsr5 <igsr5.dev@gmail.com>
2023-02-05 17:30:17 +0900

    Add log_test.go

---
+++ internal/server/log_test.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"testing"
+)
+
+func TestNewLog(t *testing.T) {
+	NewLog()
+}
+
+func TestAppend(t *testing.T) {
+	log := NewLog()
+	r := Record{
+		Value: []byte{},
+	}
+
+	_, err := log.Append(r)
+	if err != nil {
+		t.Fatalf("failed to append %s", err)
+	}
+}
