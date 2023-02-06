b021cc6bb70340ffa3970c9a5dd308f5832997b4
igsr5 <igsr5.dev@gmail.com>
2023-02-06 10:55:22 +0900

    Add test for Log.Read()

--- internal/server/log_test.go
+++ internal/server/log_test.go
@@ -15,7 +15,24 @@ func TestAppend(t *testing.T) {
 	}
 
 	_, err := log.Append(r)
-	if err == nil {
+	if err != nil {
 		t.Fatalf("failed to append %v", err)
+	}
+}
+
+func TestRead(t *testing.T) {
+	log := NewLog()
+	r := Record{
+		Value: []byte{},
+	}
+
+	offset, err := log.Append(r)
+	if err != nil {
+		t.Fatalf("failed to append %v", err)
+	}
+
+	r, err = log.Read(offset)
+	if err != nil {
+		t.Fatalf("failed to read. err: %v", err)
 	}
 }
