f367d7d31fb3d9a7a10297339214ab44de6bc2e8
igsr5 <igsr5.dev@gmail.com>
2023-02-09 21:48:09 +0900

    Add internal/server/http.go

---
+++ internal/server/http.go
@@ -0,0 +1,71 @@
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func NewHTTPServer(addr string) *http.Server {
+	lsvr := &logServer{
+		Log: NewLog(),
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+		} else if r.Method == "POST" {
+			lsvr.handleProduce(w, r)
+		}
+	})
+
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: mux,
+	}
+
+	return srv
+}
+
+type ProduceRequest struct {
+	Record Record `json:"record"`
+}
+
+type ProduceResponse struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeRequest struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeResponse struct {
+	Record Record `json:"record"`
+}
+
+type logServer struct {
+	Log *Log
+}
+
+func (s *logServer) handleProduce(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	var req ProduceRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	off, err := s.Log.Append(req.Record)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	res := ProduceResponse{Offset: off}
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	return
+}
